49ca9273f86651b39ae715d16451d148015bf0f1
Juanma Garrido <juangpdev@gmail.com>
2024-07-18 21:45:49 +0200

    añado cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Permitir solicitudes solo desde este origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(PORT);
 }
 bootstrap();
